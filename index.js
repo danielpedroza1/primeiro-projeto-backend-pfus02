48a3503d7dd04aba9eba230643340964c00e95c1
danielpedroza1 <daniel.pedroza@aluno.senai.br>
2025-09-18 17:25:03 -0300

    criando função login

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 const express = require('express')
-
 const app = express()
-
 const port = 5000
 
 const path = require('path')
 const caminho = path.join(__dirname, "Views")
 
+// Importações
+// Importa as rotas de usuário
+const userRoutes= require("./routes/userRoutes")
+
+// Interpretador de json, pra tratar as informações do body
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+// Cria uma rota principal para as sub rotas de usuário
+app.use("/usuarios",userRoutes)
+
+// Definindo o ejs como template engine
+app.set('view engine', 'ejs')
+
+// Definindo 'atalho' onde buscar as views
+app.set("views", path.join(__dirname, "views"))
+
+//rota de página inicial
 app.get("/home", (req,res) => {
     res.status(200)
-    res.sendFile(`${caminho}/index.html`)
+    res.render("index")
 })
 
-app.get("/pokemon", (req,res) => {
-     res.status(200)
-    res.send("Charizard")
-})
-
+//rota para quando tentar acessar uma rota que não existe
 app.use((req,res) => {
     res.status(404)
-    res.sendFile(`${caminho}/404.html`)
+    res.render("404")
 })
 
+//Rota inicial do projeto
 app.get("/", (req,res) => {
     res.status(200).send("Olá, parabéns você conseguiu")
 })
 
+//subir o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
     
---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+// Importar o json para servir como banco de dados
+const db = require("../data/db.json")
+
+// Variável pra armazenar os usuários vindos do db
+let listaUsuarios = db.usuarios
+
+module.exports = {
+    // Login
+    // Função para válidar o login
+    login : (usuario,senha) => {
+        // Busca na lista de usuários, se tem aquele usuário com as informações que ele passou
+        let logado = listaUsuarios.find((user)=>{user.email === usuario && user.senha === senha}) || null
+        return logado
+    }
+}
